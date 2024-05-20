bfe09c427b19f1550ddfa747196ab58717ae8703
WBTheshy <2933256546@qq.com>
2024-05-20 23:06:18 +0800

    Site updated: 2024-05-20 23:06:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/27/hello-world/","2024/03/27/新的开始/"];function toRandomPost(){
+var posts=["2024/05/20/520/","2024/03/27/新的开始/","2024/03/27/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
